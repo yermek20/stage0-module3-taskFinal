0c59baa85a5f413cadca908b890d86f0f064abb7
Yermek <yermek20@gmail.com>
2023-03-17 01:24:50 +0600

    Final module3

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double fahrenheit;
+        fahrenheit =(((double) temperatureCelsius*9)/5)+32;
+        System.out.println(fahrenheit);
     }
 }
